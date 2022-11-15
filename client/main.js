5680929758380e30c940b21d6eaeb00f24b85a8f
ko-yerin <goyelin@KoCharm.local>
2022-11-15 13:57:07 +0900

    client 라우터나눔

--- client/main.js
+++ client/main.js
@@ -1,4 +1,6 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-
 import './main.html';
+import './user.js'
+import './attendance.js'
+import './admin.js'
