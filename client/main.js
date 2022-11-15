840d0933ea046a34c681422ad5796bebb56efad6
ko-yerin <goyelin@KoCharm.local>
2022-11-15 15:52:55 +0900

    imports 그냥 client에서 빼서 라우터 나눔

---
+++ client/main.js
@@ -0,0 +1,6 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import './main.html';
+import '../imports/attendance/attendance'
+import '../imports/admin/admin'
+import '../imports/user/user'
--- client/user/user.js
+++
@@ -1,5 +0,0 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-import './user.html';
-import '../attendance/attendance'
-import '../admin/admin'
