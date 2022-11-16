9bce1b87a6c3ed07649a43c14d192139923735a6
ko-yerin <goyelin@KoCharm.local>
2022-11-16 15:01:56 +0900

    gitignore설정

--- server/main.js
+++ server/main.js
@@ -1,19 +1,3 @@
-import { Meteor } from 'meteor/meteor';
-import { Users } from "../lib/collection"
-
-
-Meteor.startup(() => {
-  if (Users.find().count() == 0) {
-    
-    for (var i = 0; i < 10; i++) {
-      Users.insert({
-        name: "name" + i,
-        password: "password" + i,
-        passwordCheck: "password" + i,
-      });
-    }
-
-    console.log(Users.find({}).fetch());
-  }
-});
+import { Meteor } from "meteor/meteor";
+import { Users } from "../lib/collection";
 
