517542973e8df78e672f683c2fbff3e58ecbd97a
ko-yerin <goyelin@KoCharm.local>
2022-11-17 15:06:16 +0900

    현재시간가져오기

--- server/main.js
+++ server/main.js
@@ -1,3 +1,23 @@
 import { Meteor } from "meteor/meteor";
-import { Users } from "../lib/collection";
+import { Users2 } from "../lib/collection";
 
+Meteor.startup(() => {
+  if (Users2.find().count() == 0) {
+    // var users = [];
+    for (var i = 0; i < 5; i++) {
+      Users2.insert({
+        name: "name" + i,
+        nickname: "nickname" + i,
+        password: "password" + i,
+        // in: now_time,
+        // out: now_time,
+      });
+    }
+    // console.log(Users.find({}));
+    console.log(Users2.find({}).fetch());
+    console.log("서버스타트");
+  } else {
+    console.log(Users2.find({}).fetch());
+    console.log("서버가 정상적");
+  }
+});
